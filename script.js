c47a46464b1bf7cfc364661165778d92cd2bd2e6
rebeccoder <rebsoptay@icloud.com>
2022-07-04 10:54:46 +0000

    added function swap turns

--- script.js
+++ script.js
@@ -4,22 +4,26 @@ const CIRCLE_CLASS = 'circle'
 const cellElements = document.querySelectorAll('[data-cell]')
 let circleTurn
 
-// this code only ever fire this event listener once. so if you clicked on a cell once it will not fire up the event again. //
+// this code only ever fire this event listener once. so if you clicked on a cell once it will not fire up the event again. 
 cellElements.forEach(cell => {
     cell.addEventListener('click', handleClick, { once: true})
 })
 
 function handleClick(e) {
     const cell = e.target
-// if its circles turn show circle board or if not show x's board//
+// if its circles turn show circle board or if not show x's board
     const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
-//placemark allows you to enter the x or circle into the cell //
     placeMark(cell, currentClass)
+    swapTurns()
+}
     // Check For Win
     // Check For Draw
-    // Switch Turns
+//placemark allows you to enter the x or circle into the cell 
+    function placeMark(cell, currentClass) {
+    cell.classList.add(currentClass)
 }
 
-function placeMark(cell, currentClass) {
-    cell.classList.add(currentClass)
+// switches turns from x and circle
+function swapTurns() {
+    circleTurn = !circleTurn
 }
