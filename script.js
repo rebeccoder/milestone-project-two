45245d59ade4cde55423b60cf84efde85b3163c1
rebeccoder <rebsoptay@icloud.com>
2022-07-04 11:05:34 +0000

    added setHoverBoard function

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const X_CLASS = 'x'
 const CIRCLE_CLASS = 'circle' 
 
 const cellElements = document.querySelectorAll('[data-cell]')
+const board = document.getElementById('board')
 let circleTurn
 
 // this code only ever fire this event listener once. so if you clicked on a cell once it will not fire up the event again. 
@@ -15,9 +16,9 @@ function handleClick(e) {
     const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
     placeMark(cell, currentClass)
     swapTurns()
+    setBoardHoverClass()
 }
-    // Check For Win
-    // Check For Draw
+
 //placemark allows you to enter the x or circle into the cell 
     function placeMark(cell, currentClass) {
     cell.classList.add(currentClass)
@@ -26,4 +27,14 @@ function handleClick(e) {
 // switches turns from x and circle
 function swapTurns() {
     circleTurn = !circleTurn
+}
+
+function setBoardHoverClass() {
+    board.classList.remove(X_CLASS)
+    board.classList.remove(CIRCLE_CLASS)
+    if (circleTurn) {
+        board.classList.add(CIRCLE_CLASS)
+    } else {
+        board.classList.add(X_CLASS)
+    }
 }
