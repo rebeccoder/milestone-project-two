48e10c01f2bdebab8ff241182e4a9a39affd7c12
rebeccoder <rebsoptay@icloud.com>
2022-07-13 20:32:52 +0000

    change winning message from wins to win to make it make more sense

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ function endGame(draw) {
     if (draw) {
         winningMessageTextElement.innerText = 'Draw!'
     } else {
-        winningMessageTextElement.innerText = `${circleTurn ? "O's" : "X's"} Wins!`
+        winningMessageTextElement.innerText = `${circleTurn ? "O's" : "X's"} Win!`
     }
     winningMessageElement.classList.add('show')
 }
